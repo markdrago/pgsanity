407e1067bc961b7bcddca3035ef14fea06e03c12
Koyae <CronoCat@hotmail.com>
2016-05-15 10:51:46 -0600

    Attempting to deal with UTF-8 BOM-table stuff.

--- pgsanity/pgsanity.py
+++ pgsanity/pgsanity.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function
 from __future__ import absolute_import
+from chardet import detect
+from codecs import BOM_UTF8
 import argparse
 import sys
 
@@ -13,6 +15,25 @@ def get_config(argv=sys.argv[1:]):
     parser.add_argument('files', nargs='*', default=None)
     return parser.parse_args(argv)
 
+def check_for_bom(starting_bytes):
+    """ Check the first few bytes of a file to determine whether input
+    contains a BOM-table or not.
+
+    Returns a boolean indicating whether a BOM-table appears to be present.
+    """
+    minlen = len(BOM_UTF8)
+    if len(starting_bytes) < minlen:
+        raise ValueError("Starting bytes of file must be at least"
+                         " {} bytes long to check for BOM.".format(minlen))
+    encoding = detect(starting_bytes)["encoding"]
+    is_utf8 = encoding in ["UTF-8","UTF-8-SIG"]
+    return is_utf8 and starting_bytes.startswith(BOM_UTF8)
+    # ^ The above is a tiny bit redundant given that 'UTF-8-SIG' simply means
+    # "UTF-8 file with a BOM-table". However, older versions of chardet don't
+    # support this, and will just detect 'UTF-8', leaving us to check for the
+    # BOM ourselves as we do above. The extra check is not harmful on
+    # systems that have a more recent chardet module.
+
 def check_file(filename=None, show_filename=False):
     """
     Check whether an input file is valid PostgreSQL. If no filename is
@@ -22,8 +43,13 @@ def check_file(filename=None, show_filename=False):
     """
     # either work with sys.stdin or open the file
     if filename is not None:
-        with open(filename, "r") as filelike:
-            sql_string = filelike.read()
+        with open(filename, "rb") as filelike:
+            # discard BOM if present then read remaining bytes
+            nose = filelike.read(len(BOM_UTF8))
+            nose = '' if check_for_bom(nose) else nose
+            sql_string = nose + filelike.read()
+            sql_string = sql_string.decode("utf-8")
+            # ^ This is safe for both ASCII and UTF-8 files.
     else:
         with sys.stdin as filelike:
             sql_string = sys.stdin.read()
